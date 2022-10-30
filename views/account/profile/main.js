c7464c55d4285a964e026521d41dd78f56df9c7d
Leah <kazenml1@gmail.com>
2022-10-30 17:18:28 -0400

    Makes it so that the profile page looks like Payton's mockup.

--- views/account/profile/main.js
+++ views/account/profile/main.js
@@ -1,22 +1,10 @@
-// JQuery Datatables Initialization
-$(document).ready(function () {
-  var table = $("#casual-table").DataTable();
-  new $.fn.DataTable.Responsive(table);
-});
-$(document).ready(function () {
-  var table = $("#professional-table").DataTable();
-  new $.fn.DataTable.Responsive(table);
-});
-
-let casual_table = document.getElementById("casual-table-body");
-let professional_table = document.getElementById("professional-table-body");
+let accounts_table = document.getElementById("accounts-table-body");
 
 // These are dummy values for now; we would need to pull the casual and professional info from the db
-let casual_data_labels = ["Instagram", "Facebook"];
-let casual_data_values = ["lelekaz", "Leah Kazenmayer"];
-
-let professional_data_labels = ["Email", "LinkedIn"];
-let professional_data_values = [
+let labels = ["Instagram", "Facebook", "Email", "LinkedIn"];
+let values = [
+  "lelekaz",
+  "Leah Kazenmayer",
   "kazenml1@tcnj.edu",
   "https://www.linkedin.com/in/leah-kazenmayer/",
 ];
@@ -25,32 +13,36 @@ function populate_table(table, labels, values) {
   for (let i = 0; i < labels.length; i++) {
     let tr = document.createElement("tr");
     let label = document.createElement("td");
+    let label_input = document.createElement("input");
+    label_input.classList.add('form-control');
     let value = document.createElement("td");
-    let action = document.createElement("td");
-    let edit_btn = document.createElement("button");
+    let value_input = document.createElement("input");
+    value_input.classList.add('form-control');
+    let delete_td = document.createElement("td");
 
-    edit_btn.innerText = "Edit";
-    edit_btn.classList.add("btn", "btn-warning", "btn-sm");
     let delete_btn = document.createElement("button");
     delete_btn.innerText = "Delete";
     delete_btn.classList.add("btn", "btn-danger", "btn-sm");
 
-    label.innerText = labels[i];
-    value.innerText = values[i];
-    action.appendChild(edit_btn);
-    action.appendChild(delete_btn);
+    delete_btn.onclick = function() {
+      if (confirm("Are you sure you want to delete this account information?")) {
+        location.reload();
+      }
+    }
+
+    label_input.value = labels[i];
+    label.appendChild(label_input);
+    value_input.value = values[i];
+    value.appendChild(value_input);
+
+    delete_td.appendChild(delete_btn);
 
     tr.appendChild(label);
     tr.appendChild(value);
-    tr.appendChild(action);
+    tr.appendChild(delete_td);
 
     table.appendChild(tr);
   }
 }
 
-populate_table(casual_table, casual_data_labels, casual_data_values);
-populate_table(
-  professional_table,
-  professional_data_labels,
-  professional_data_values
-);
+populate_table(accounts_table, labels, values);
