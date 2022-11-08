4c5fe9ccad2133c78074bb4c3f5738f6127fa356
Leah <kazenml1@gmail.com>
2022-11-08 00:13:33 -0500

    Creates the page that the scanner lands on to see the person's profile.

--- server.js
+++ server.js
@@ -41,6 +41,7 @@ require("./routes/signup")(app, viewsPath);
 require("./routes/profile")(app, viewsPath);
 require("./routes/settings")(app, viewsPath);
 require("./routes/aboutus")(app, viewsPath);
+require("./routes/displaycard")(app, viewsPath);
 
 // Redirect any other unknown requests to the home page
 app.get("*", (req, res) => {
