d1ac401e53a1cc471d4171c0125b1b9ca011d311
Leah <kazenml1@gmail.com>
2022-11-08 13:41:37 -0500

    Making margin-top rem instead of px in notfoundpage style.css.

--- views/account/profile/main.js
+++ views/account/profile/main.js
@@ -15,7 +15,6 @@ const settings = await (
   await fetch("/account/profile/getsettings", { method: "GET" })
 ).json();
 
-
 var htmlElement = document.getElementById("html");
 htmlElement.style.fontSize = settings.font_size;
 htmlElement.style.fontFamily = settings.font_family;
