6e66811ba3fd3ed0c7ebf420ae3c26da489d0297
Sterly Deracy <deracys1@tcnj.edu>
2022-11-10 18:24:40 -0500

    Implement light mode settings throughout other pages

--- views/aboutus/main.js
+++ views/aboutus/main.js
@@ -8,6 +8,47 @@ fetch("/account/profile/getsettings", { method: "GET" })
       "--bs-body-font-family: " + settings.font_family
     );
     htmlElement.style.fontSize = settings.font_size;
+
+    const div = document.getElementById("body"); // Get element from DOM
+    const contentDiv = document.getElementById("content-div");
+    const buttons = document.querySelectorAll(".btn");
+    if (settings.theme === "Light Theme") {
+      div.classList.remove("dark-mode");
+      div.classList.remove("bg-dark");
+      div.classList.remove("text-white");
+      div.classList.remove("navbar-dark");
+      div.classList.add("light-mode");
+      div.classList.add("text-navy");
+
+      contentDiv.classList.remove("text-white");
+      contentDiv.classList.add("text-navy");
+
+      buttons.forEach((button) => {
+        if (button.classList.contains("btn-warning")) {
+          button.classList.remove("btn-warning");
+          button.classList.add("btn-dark");
+          button.classList.add("bg-navy");
+        }
+      });
+    } else {
+      div.classList.add("dark-mode");
+      div.classList.add("bg-dark");
+      div.classList.add("text-white");
+      div.classList.add("navbar-dark");
+      div.classList.remove("light-mode");
+      div.classList.remove("text-navy");
+
+      contentDiv.classList.remove("text-navy");
+      contentDiv.classList.add("text-white");
+
+      buttons.forEach((button) => {
+        if (button.classList.contains("btn-dark")) {
+          button.classList.remove("btn-dark");
+          button.classList.remove("bg-navy");
+          button.classList.add("btn-warning");
+        }
+      });
+    }
   })
   .catch((error) => {
     console.error(error);
