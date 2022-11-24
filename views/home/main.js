95b4814b6c99cced0981951882f7350774219698
nishatreddy <thadakn1@tcnj.edu>
2022-11-23 22:31:52 -0500

    Help feature on interactive pages

--- views/home/main.js
+++ views/home/main.js
@@ -365,3 +365,18 @@ if ("webkitSpeechRecognition" in window) {
 } else {
   console.log("Speech Recognition Not Available");
 }
+
+let modalBtn = document.getElementById("modal-btn");
+let modal = document.querySelector(".modal");
+let closeBtn = document.querySelector(".close-btn");
+modalBtn.onclick = function () {
+  modal.style.display = "block";
+};
+closeBtn.onclick = function () {
+  modal.style.display = "none";
+};
+window.onclick = function (e) {
+  if (e.target == modal) {
+    modal.style.display = "none";
+  }
+};
